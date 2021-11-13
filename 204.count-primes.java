48ee34a05891380e4e2ba96f8d9d883a500fcc5d
Jasdeep Singh <jasdeepsingh2405@gmail.com>
2021-11-13 13:55:12 +0530

    count primes

---
+++ 204.count-primes.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=204 lang=java
+ *
+ * [204] Count Primes
+ */
+
+// @lc code=start
+class Solution {
+    public int countPrimes(int n) {
+        if (n < 2)
+            return 0;
+        boolean[] primes = new boolean[n];
+        Arrays.fill(primes, true);
+        for (int i = 2; i * i < primes.length; i++) {
+            for (int j = i; j * i < primes.length; j++) {
+                primes[i * j] = false;
+            }
+        }
+        int c = 0;
+        for (int i = 2; i < primes.length; i++) {
+            if (primes[i])
+                c++;
+        }
+        return c;
+    }
+}
+// @lc code=end

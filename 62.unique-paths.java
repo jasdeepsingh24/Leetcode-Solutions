c1385cca2499336dd6b7498deef82a25423284a2
Jasdeep Singh <jasdeepsingh2405@gmail.com>
2022-02-06 18:22:36 +0530

    unique path

---
+++ 62.unique-paths.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=62 lang=java
+ *
+ * [62] Unique Paths
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++)
+            a[i][n - 1] = 1;
+        for (int j = 0; j < n; j++)
+            a[m - 1][j] = 1;
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                a[i][j] = a[i + 1][j] + a[i][j + 1];
+            }
+        }
+        return a[0][0];
+
+    }
+}
+// @lc code=end

5d386ef56135ca2539259e6404598d55c3670d53
Jasdeep Singh <jasdeepsingh2405@gmail.com>
2022-02-23 00:49:03 +0530

    merge bt

---
+++ 617.merge-two-binary-trees.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=617 lang=java
+ *
+ * [617] Merge Two Binary Trees
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        
+    }
+}
+// @lc code=end
+

bb82375b22b93cc6add68df3b83c45236d5f0ede
Jasdeep Singh <jasdeepsingh2405@gmail.com>
2022-02-24 00:54:57 +0530

    updated coin change

--- 322.coin-change.java
+++ 322.coin-change.java
@@ -8,27 +8,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[][] dp = new int[coins.length + 1][amount + 1];
-        for (int j = 0; j < dp[0].length; j++)
-            dp[0][j] = -1;
-        for (int i = 1; i < dp.length; i++) {
+        for (int j = 1; j < dp[0].length; j++)
+            dp[0][j] = Integer.MAX_VALUE - 1;
+        for (int j = 1; j < dp[0].length; j++) {
+            if (j % coins[0] == 0)
+                dp[1][j] = j / coins[0];
+            else
+                dp[1][j] = Integer.MAX_VALUE - 1;
+        }
+        for (int i = 2; i < dp.length; i++) {
             for (int j = 1; j < dp[0].length; j++) {
-                if (coins[i - 1] <= j) {
-                    if (dp[i - 1][j] != -1) {
-                        if (dp[i][j - coins[i - 1]] != -1)
-                            dp[i][j] = Math.min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j]);
-                        else
-                            dp[i][j] = dp[i - 1][j];
-                    } else {
-                        if (dp[i][j - coins[i - 1]] != -1)
-                            dp[i][j] = dp[i][j - coins[i - 1]] + 1;
-                        else
-                            dp[i][j] = -1;
-                    }
-                } else
+                if (coins[i - 1] > j)
                     dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i - 1]] + 1);
             }
         }
-        return dp[dp.length - 1][dp[0].length - 1];
+        return dp[dp.length - 1][dp[0].length - 1] == Integer.MAX_VALUE - 1 ? -1 : dp[dp.length - 1][dp[0].length - 1];
     }
+
 }
 // @lc code=end

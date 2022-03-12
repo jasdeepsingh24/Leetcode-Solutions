d73d98596787ba9ee73e35880ec6543539a72fe9
Jasdeep Singh <jasdeepsingh2405@gmail.com>
2022-03-12 17:43:25 +0530

    longest consecutive

---
+++ 1286.iterator-for-combination.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1286 lang=java
+ *
+ * [1286] Iterator for Combination
+ */
+
+// @lc code=start
+class CombinationIterator {
+
+    public CombinationIterator(String characters, int combinationLength) {
+        
+    }
+    
+    public String next() {
+        
+    }
+    
+    public boolean hasNext() {
+        
+    }
+}
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator obj = new CombinationIterator(characters, combinationLength);
+ * String param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+// @lc code=end
+
